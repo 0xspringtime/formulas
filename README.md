af586811e5db16b9141c6acda539ad9d1cc0327d
0xspringtime <0xspringtime@protonmail.com>
2023-05-26 13:20:40 -0400

    commit

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Covariance:
 
 $${cov}(X, Y) = \frac{1}{n} \sum_{i=1}^{n} (x_i - \bar{x})(y_i - \bar{y})$$
 
-[Pearson's r](./pearsonr.py)
+[Pearson's r:](./pearsonr.py)
 
-$r = \frac{\text{cov}(X, Y)}{\sigma_X \sigma_Y}$
+$$r = \frac{\text{cov}(X, Y)}{\sigma_X \sigma_Y}$$
+
+
---
+++ stdev.py
@@ -0,0 +1,40 @@
+# Sample data
+data = [1, 2, 3, 4, 5]
+
+# Calculate mean
+mean = sum(data) / len(data)
+
+# Calculate squared differences from the mean
+squared_diff = [(x - mean) ** 2 for x in data]
+
+# Calculate variance
+variance = sum(squared_diff) / len(data)
+
+# Calculate standard deviation
+std_dev = variance ** 0.5
+
+# Print the standard deviation
+print("Standard Deviation:", std_dev)
+
+import statistics
+
+# Sample data
+data = [1, 2, 3, 4, 5]
+
+# Calculate standard deviation
+std_dev = statistics.stdev(data)
+
+# Print the standard deviation
+print("Standard Deviation (with library):", std_dev)
+
+import numpy as np
+
+# Sample data
+data = [1, 2, 3, 4, 5]
+
+# Calculate standard deviation
+std_dev = np.std(data)
+
+# Print the standard deviation
+print("Standard Deviation (with library):", std_dev)
+
